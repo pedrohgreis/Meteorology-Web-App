e55bda1dae264426728015be258183785ee5c47d
Pedro <pedrohgdreis@gmail.com>
2025-04-03 13:21:16 -0400

    feat: Header updated

--- meteorology/src/Pages/Home.tsx
+++ meteorology/src/Pages/Home.tsx
@@ -1,8 +1,10 @@
-import { Header } from "../Components/Header";
+
 
 export const Home = () => {
 
     return(
-        <Header />
+        <div>
+            <h1>Home</h1>
+        </div>
     );
 };
--- meteorology/src/Router.tsx
+++ meteorology/src/Router.tsx
@@ -2,11 +2,15 @@ import {Routes, Route} from "react-router-dom";
 
 //Pages
 import {Home} from "./Pages/Home";
+import { DefaultLayout } from "./Layouts/DefaulLayout";
 
 export const Router = () => {
     return(
         <Routes>
-            <Route path='/' element={<Home />} />
+            <Route path="/" element={<DefaultLayout />}>
+                <Route path='/' element={<Home />} />
+            </Route>
         </Routes>
+        
     );
 };
